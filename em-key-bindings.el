d6f3e43182b025f282dc7191c8a570c369ddf634
weiqiyiji <weiqiyiji@gmail.com>
2012-10-17 11:04:47 +0800

    Add ace-jump-mode key-binding

--- em-key-bindings.el
+++ em-key-bindings.el
@@ -2,8 +2,6 @@
 (global-set-key (kbd "C-z") 'ctrl-z-map)
 (global-set-key (kbd "C-z C-m") 'set-mark-command)
 
-(global-set-key (kbd "C-<f10>") 'menu-bar-mode)
-
 ;; Font size
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
@@ -17,17 +15,20 @@
 ;; Window
 (global-set-key (kbd "C-x C-n") 'other-window)
 (global-set-key (kbd "C-x C-p") 'other-window-backward)
-
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
 
 (global-set-key (kbd "M-w") 'kill-ring-save)
 (global-set-key (kbd "M-t") 'textmate-goto-file)
 
 (global-set-key (kbd "<f5>") 'revert-buffer)
+(global-set-key (kbd "C-<f10>") 'menu-bar-mode)
+
+(define-key emacs-lisp-mode-map (kbd "C-c C-f") 'find-function)
 
 (when (functionp 'ns-toggle-fullscreen)
   (global-set-key (kbd "C-x F") 'ns-toggle-fullscreen))
 
-(define-key emacs-lisp-mode-map (kbd "C-c C-f") 'find-function)
+(when (cabbage-bundle-active-p 'power-edit)
+  (global-set-key (kbd "C-c SPC") 'ace-jump-mode))
 
 (provide 'em-key-bindings)
