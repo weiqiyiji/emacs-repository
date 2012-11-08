fe560f73b11219085fae2b5cc383acb0908556c0
weiqiyiji <weiqiyiji@gmail.com>
2012-11-08 16:21:16 +0800

    Add MMs java file auto header

--- bundles/autoinsert/bundle.el
+++ bundles/autoinsert/bundle.el
@@ -7,4 +7,20 @@
 (define-auto-insert "\.py" "python.tpl")
 (define-auto-insert "\.sh" "shell.tpl")
 
+(setq current-date-format "%Y-%m-%d")
+
+(define-auto-insert '("\\.java\\'" . "MMs Java skeleton")
+  '("Short description: "
+    "/**" "\n"
+    " * @(#)"
+    (buffer-name)
+    " " (format-time-string current-date-format (current-time)) ".\n"
+    " *\n"
+    " * Copyright (c) 2011, Wandou Labs and/or its affiliates. All rights reserved.\n"
+    " * WANDOU LABS PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.\n"
+    " */" "\n \n \n"
+    "/**" "\n"
+    " * @author luoji" "\n"
+    " */"))
+
 (add-hook 'find-file-hooks 'auto-insert)
